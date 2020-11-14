8c6efd64be0ffe0faf973d6cdb792ca7b7df6580
FanJups <jupsfan@gmail.com>
2020-11-14 21:24:38 +0100

    textarea

--- src/main/java/fan/jups/javafx/App.java
+++ src/main/java/fan/jups/javafx/App.java
@@ -1,12 +1,14 @@
 package fan.jups.javafx;
 
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -14,38 +16,38 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
 
-	// Create the Selection Label
-	Label selectionLbl = new Label("Your selection: None");
+	// Create the Message TextArea
+	TextArea messageArea = new TextArea();
 
 	@Override
 	public void start(Stage stage) {
 
-		// Create the ListView
-		ListView<String> cars = new ListView<String>();
-		// Add Items to the ListView
-		cars.getItems().addAll("Ford", "Audi", "Ferrari", "Porsche");
-		// Select the first car from the list
-		cars.getSelectionModel().selectFirst();
-		// Add ChangeListener to the ListView
-		cars.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
-			public void changed(ObservableValue<? extends String> ov, final String oldValue, final String newValue) {
-				selectionLbl.setText("Your selection: " + newValue);
+		// Create the TextField for the Input
+		final TextField input = new TextField();
+		input.setPromptText("Input your message here");
+		// Set the Prompt and Size of the TextArea
+		messageArea.setPromptText("Your Message:");
+		messageArea.setPrefColumnCount(20);
+		messageArea.setPrefRowCount(10);
+		// Create the Print Button
+		Button printBtn = new Button("Print Message");
+		// Add an EvenetHandler to the Button
+		printBtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				messageArea.appendText(input.getText() + "\n");
 			}
 		});
-		// Create the GridPane
-		GridPane root = new GridPane();
-		// Set the horizontal and vertical spacing between columns and rows
-		root.setVgap(10);
-		root.setHgap(10);
-		// Add ListView and Label to the GridPane
-		root.addRow(0, cars);
-		root.addRow(1, selectionLbl);
-		// Set the Size of the GridPane
-		root.setMinSize(300, 200);
+		// Create the VBox
+		VBox root = new VBox();
+		// Add Labels, TextArea and TextField to the VBox
+		root.getChildren().addAll(new Label("Input:"), input, new Label("Messages:"), messageArea, printBtn);
+		// Set the Size of the VBox
+		root.setMinSize(350, 250);
 		/*
-		 * Set the padding of the GridPane Set the border-style of the GridPane Set the
-		 * border-width of the GridPane Set the border-insets of the GridPane Set the
-		 * border-radius of the GridPane Set the border-color of the GridPane
+		 * Set the padding of the VBox Set the border-style of the VBox Set the
+		 * border-width of the VBox Set the border-insets of the VBox Set the
+		 * border-radius of the VBox Set the border-color of the VBox
 		 */
 		root.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;" + "-fx-border-width: 2;"
 				+ "-fx-border-insets: 5;" + "-fx-border-radius: 5;" + "-fx-border-color: blue;");
@@ -54,7 +56,7 @@ public class App extends Application {
 		// Add the scene to the Stage
 		stage.setScene(scene);
 		// Set the title of the Stage
-		stage.setTitle("A ListView Example");
+		stage.setTitle("A TextArea Example");
 		// Display the Stage
 		stage.show();
 	}
