693a6ad086d98d3afe9533446478538a56740d86
FanJups <jupsfan@gmail.com>
2020-11-14 21:00:43 +0100

    combobox

--- src/main/java/fan/jups/javafx/App.java
+++ src/main/java/fan/jups/javafx/App.java
@@ -1,10 +1,13 @@
 package fan.jups.javafx;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -13,37 +16,43 @@ import javafx.stage.Stage;
 public class App extends Application {
 
 	// Create the Selection Label
-	Label selectionMsg = new Label("Your selection: None");
+	Label selectionLbl = new Label("Your selection: None");
 
 	@Override
 	public void start(Stage stage) {
 
 		// Create the Label for the Car
 		Label carLbl = new Label("Car:");
-		// Create a ChoiceBox for cars
-		ChoiceBox<String> cars = new ChoiceBox<>();
-		// Add the items to the ChoiceBox
+		// Create a ComboBox for cars
+		ComboBox<String> cars = new ComboBox<>();
+		// Add the items to the ComboBox
 		cars.getItems().addAll("Ford", "Audi", "Ferrari", "Porsche");
-		// Create the Selection Message Label
-		Label selectionMsgLbl = new Label("Your selection:");
-		// Create the Selection Value Label
-		Label selectedValueLbl = new Label();
-		// Bind the value property to the text property of the Label
-		selectedValueLbl.textProperty().bind(cars.valueProperty());
-		// Display controls in a GridPane
-		GridPane root = new GridPane();
-		// Set the spacing between columns and rows
-		root.setVgap(10);
-		root.setHgap(10);
-		// Add the Labels and the ChoiceBox to the GridPane
-		root.addRow(0, carLbl, cars);
-		root.addRow(1, selectionMsgLbl, selectedValueLbl);
-		// Set the Size of the GridPane
+		// Select the first car from the list
+		cars.getSelectionModel().selectFirst();
+		// Add a ChangeListener to the ComboBox
+		cars.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+			public void changed(ObservableValue<? extends String> ov, final String oldValue, final String newValue) {
+				selectionLbl.setText("Your selection: " + newValue);
+			}
+		});
+		// Create the HBox
+		HBox carbox = new HBox();
+		// Add the children to the HBox
+		carbox.getChildren().addAll(carLbl, cars);
+		// Set the vertical spacing between children to 10px
+		carbox.setSpacing(10);
+		// Create the VBox
+		VBox root = new VBox();
+		// Add the children to the VBox
+		root.getChildren().addAll(carbox, selectionLbl);
+		// Set the vertical spacing between children to 10px
+		root.setSpacing(10);
+		// Set the Size of the VBox
 		root.setMinSize(350, 250);
 		/*
-		 * Set the padding of the GridPane Set the border-style of the GridPane Set the
-		 * border-width of the GridPane Set the border-insets of the GridPane Set the
-		 * border-radius of the GridPane Set the border-color of the GridPane
+		 * Set the padding of the VBox Set the border-style of the VBox Set the
+		 * border-width of the VBox Set the border-insets of the VBox Set the
+		 * border-radius of the VBox Set the border-color of the VBox
 		 */
 		root.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;" + "-fx-border-width: 2;"
 				+ "-fx-border-insets: 5;" + "-fx-border-radius: 5;" + "-fx-border-color: blue;");
@@ -52,7 +61,7 @@ public class App extends Application {
 		// Add the scene to the Stage
 		stage.setScene(scene);
 		// Set the title of the Stage
-		stage.setTitle("A ChoiceBox Example");
+		stage.setTitle("A ComboBox Example");
 		// Display the Stage
 		stage.show();
 	}
