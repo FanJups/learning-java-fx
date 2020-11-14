5336366ce0d7da05578650fb3bc5724dc26cffea
FanJups <jupsfan@gmail.com>
2020-11-14 20:32:32 +0100

    radiobutton

--- src/main/java/fan/jups/javafx/App.java
+++ src/main/java/fan/jups/javafx/App.java
@@ -5,6 +5,7 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
@@ -22,35 +23,36 @@ public class App extends Application {
 
 	@Override
 	public void start(Stage stage) {
-
-		// Create four ToggleButtons
-		ToggleButton fordBtn = new ToggleButton("Ford");
-		ToggleButton audiBtn = new ToggleButton("Audi");
-		ToggleButton ferrariBtn = new ToggleButton("Ferrari");
-		ToggleButton porscheBtn = new ToggleButton("Porsche");
+		// Create the RadioButtons
+		RadioButton fordBtn = new RadioButton("Ford");
+		RadioButton audiBtn = new RadioButton("Audi");
+		RadioButton ferrariBtn = new RadioButton("Ferrari");
+		RadioButton porscheBtn = new RadioButton("Porsche");
 		// Create a ToggleGroup
-		final ToggleGroup group = new ToggleGroup();
-		// Add all ToggleButtons to a ToggleGroup
+		ToggleGroup group = new ToggleGroup();
+		// Add all RadioButtons to a ToggleGroup
 		group.getToggles().addAll(fordBtn, audiBtn, ferrariBtn, porscheBtn);
-		// Create a ChangeListener for the ToggleGroup
+		// Add a listener to the ToggleGroup
 		group.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
 			public void changed(ObservableValue<? extends Toggle> ov, final Toggle toggle, final Toggle new_toggle) {
 				String toggleBtn = ((ToggleButton) new_toggle).getText();
 				selectionMsg.setText("Your selection: " + toggleBtn);
 			}
 		});
-		// Create the Label for the Selection
-		Label selectLbl = new Label("Select the car you like:");
+		// Select the default car as ferrari
+		ferrariBtn.setSelected(true);
+		// Create the Selection Label
+		Label msg = new Label("Select the car you like the most:");
 		// Create a HBox
 		HBox buttonBox = new HBox();
-		// Add ToggleButtons to an HBox
+		// Add RadioButtons to an HBox
 		buttonBox.getChildren().addAll(fordBtn, audiBtn, ferrariBtn, porscheBtn);
 		// Set the spacing between children to 10px
 		buttonBox.setSpacing(10);
 		// Create the VBox
 		VBox root = new VBox();
-		// Add the Labels and HBox to the VBox
-		root.getChildren().addAll(selectionMsg, selectLbl, buttonBox);
+		// Add Labels and RadioButtons to an VBox
+		root.getChildren().addAll(selectionMsg, msg, buttonBox);
 		// Set the spacing between children to 10px
 		root.setSpacing(10);
 		// Set the Size of the VBox
@@ -58,24 +60,18 @@ public class App extends Application {
 		/*
 		 * Set the padding of the VBox Set the border-style of the VBox Set the
 		 * border-width of the VBox Set the border-insets of the VBox Set the
-		 * border-radius of the VBox Set the border-color of the VBox
+		 * border-radius of the VBox * Set the border-color of the VBox
 		 */
 		root.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;" + "-fx-border-width: 2;"
 				+ "-fx-border-insets: 5;" + "-fx-border-radius: 5;" + "-fx-border-color: blue;");
-		// Create the Scene
+// Create the Scene
 		Scene scene = new Scene(root);
-		// Add the scene to the Stage
+// Add the scene to the Stage
 		stage.setScene(scene);
-		// Set the title of the Stage
-		stage.setTitle("A ToggleButton Example");
-		// Display the Stage
+// Set the title of the Stage
+		stage.setTitle("A RadioButton Example");
+// Display the Stage
 		stage.show();
-	}
-
-	// Helper Method
-	public void printMessage(String message) {
-		// Set the Text of the Label
-		selectionMsg.setText(message);
 	}
 
 	public static void main(String[] args) {
