6461d6e7afff5d3766daef0fa05a4cb46a7a6670
FanJups <jupsfan@gmail.com>
2020-11-14 19:25:44 +0100

    button

--- src/main/java/fan/jups/javafx/App.java
+++ src/main/java/fan/jups/javafx/App.java
@@ -4,9 +4,10 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -15,56 +16,71 @@ import javafx.stage.Stage;
 public class App extends Application {
 
 	// Create the Message Label
-	Label messageLbl = new Label("Enter your Name into the Text Fields.");
+	Label messageLbl = new Label("Press any Button to see the message");
 
 	@Override
 	public void start(Stage stage) {
 
-		// Create the TextFields
-		TextField firstNameFld = new TextField();
-		TextField lastNameFld = new TextField();
-		// Both fields should be wide enough to display 15 chars
-		firstNameFld.setPrefColumnCount(15);
-		lastNameFld.setPrefColumnCount(15);
-		// Set ActionEvent handlers for both fields
-		firstNameFld.setOnAction(new EventHandler<ActionEvent>() {
+		// Create a normal button with N as its mnemonic
+		Button newBtn = new Button("_New");
+		// Add EventHandler to the Button
+		newBtn.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
-				printMessage("You have changed the First Name!");
+				printMessage("You have pressed the new Button");
 			}
 		});
-		lastNameFld.setOnAction(new EventHandler<ActionEvent>() {
+		// Create a default button with S as its mnemonic
+		Button saveBtn = new Button("_Save");
+		// Set this Button as the Default
+		saveBtn.setDefaultButton(true);
+		// Add EventHandler to the Button
+		saveBtn.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
-				printMessage("You have changed the Last Name !");
+				printMessage("You have pressed the save Button");
 			}
 		});
-		GridPane root = new GridPane();
-		// Set the horizontal spacing to 10px
-		root.setHgap(10);
-		// Set the vertical spacing to 5px
-		root.setVgap(5);
-		// Add Labels and Fields to the GridPane
-		root.addRow(0, messageLbl);
-		root.addRow(1, new Label("First Name:"), firstNameFld);
-		root.addRow(2, new Label("Last Name:"), lastNameFld);
-		// Set the Size of the GridPane
+		// Create a cancel button with C as its mnemonic
+		Button cancelBtn = new Button("_Cancel");
+		cancelBtn.setCancelButton(true);
+		// Add EventHandler to the Button
+		cancelBtn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				printMessage("You have pressed the cancel Button");
+			}
+		});
+		// Create the HBox
+		HBox buttonBox = new HBox();
+		// Add the children to the HBox
+		buttonBox.getChildren().addAll(newBtn, saveBtn, cancelBtn);
+		// Set the vertical spacing between children to 15px
+		buttonBox.setSpacing(15);
+		// Create the VBox
+		VBox root = new VBox();
+		// Add the children to the VBox
+		root.getChildren().addAll(messageLbl, buttonBox);
+		// Set the vertical spacing between children to 15px
+		root.setSpacing(15);
+		// Set the Size of the VBox
 		root.setMinSize(350, 250);
 		/*
-		 * Set the padding of the GridPane Set the border-style of the GridPane Set the
-		 * border-width of the GridPane Set the border-insets of the GridPane Set the
-		 * border-radius of the GridPane Set the border-color of the GridPane
+		 * Set the padding of the VBox Set the border-style of the VBox Set the
+		 * border-width of the VBox * Set the border-insets of the VBox Set the
+		 * border-radius of the VBox Set the border-color of the VBox
 		 */
 		root.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;" + "-fx-border-width: 2;"
 				+ "-fx-border-insets: 5;" + "-fx-border-radius: 5;" + "-fx-border-color: blue;");
-		// Create the Scene
+// Create the Scene
 		Scene scene = new Scene(root);
-		// Add the scene to the Stage
+// Add the scene to the Stage
 		stage.setScene(scene);
-		// Set the title of the Stage
-		stage.setTitle("A TextField Example");
-		// Display the Stage
+// Set the title of the Stage
+		stage.setTitle("A Button Example");
+// Display the Stage
 		stage.show();
+
 	}
 
 	// Helper Method
