6e79670230aaa7e0856c8f67329041fa9b73e16b
FanJups <jupsfan@gmail.com>
2020-11-14 19:57:27 +0100

    checkbox

--- src/main/java/fan/jups/javafx/App.java
+++ src/main/java/fan/jups/javafx/App.java
@@ -1,12 +1,11 @@
 package fan.jups.javafx;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -15,80 +14,70 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
 
-	// Create the Message Label
-	Label messageLbl = new Label("Choose your car!");
+	// Create the Selection Label
+	Label selectionMsg = new Label("Choose your Car");
 
 	@Override
 	public void start(Stage stage) {
 
-		// Create the MenuItem ford
-		MenuItem ford = new MenuItem("Ford");
-		// Add EventHandler to the MenuItem
-		ford.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent e) {
-				printMessage("You have selected: Ford");
+		// Create a CheckBox to support only two states
+		CheckBox fordCbx = new CheckBox("Ford");
+		// Create a CheckBox to support three states
+		CheckBox audiCbx = new CheckBox("Audi");
+		audiCbx.setAllowIndeterminate(true);
+		// Add a ChangeListener to the CheckBox fordCbx
+		fordCbx.selectedProperty().addListener(new ChangeListener<Boolean>() {
+			public void changed(ObservableValue<? extends Boolean> ov, final Boolean value, final Boolean newValue) {
+				if (newValue != null && newValue) {
+					printMessage("Your Selection: Ford");
+				}
 			}
 		});
-		// Create the MenuItem audi
-		MenuItem audi = new MenuItem("Audi");
-		// Add EventHandler to the MenuItem
-		audi.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent e) {
-				printMessage("You have selected: Audi");
+		// Add a ChangeListener to the CheckBox audiCbx
+		audiCbx.selectedProperty().addListener(new ChangeListener<Boolean>() {
+			public void changed(ObservableValue<? extends Boolean> ov, final Boolean value, final Boolean newValue) {
+				if (newValue != null && newValue) {
+					printMessage("Your Selection: Audi");
+				}
 			}
 		});
-		// Create the MenuItem ferrari
-		MenuItem ferrari = new MenuItem("Ferrari");
-		// Add EventHandler to the MenuItem
-		ferrari.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent e) {
-				printMessage("You have selected: Ferrari");
+		// Add a ChangeListener to the CheckBox audiCbx
+		audiCbx.indeterminateProperty().addListener(new ChangeListener<Boolean>() {
+			public void changed(ObservableValue<? extends Boolean> ov, final Boolean value, final Boolean newValue) {
+				if (newValue != null && newValue) {
+					printMessage("Your indeterminate Selection: Audi");
+				}
 			}
 		});
-		// Create the MenuItem porsche
-		MenuItem porsche = new MenuItem("Porsche");
-		// Add EventHandler to the MenuItem
-		porsche.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent e) {
-				printMessage("You have selected: Porsche");
-			}
-		});
-		// Create the MenuButton
-		MenuButton cars = new MenuButton("Select");
-		// Add menu items to the MenuButton
-		cars.getItems().addAll(ford, audi, ferrari, porsche);
 		// Create the VBox
 		VBox root = new VBox();
 		// Add the children to the VBox
-		root.getChildren().addAll(cars, messageLbl);
+		root.getChildren().addAll(selectionMsg, fordCbx, audiCbx);
+		// Set the vertical spacing between children to 20px
+		root.setSpacing(20);
 		// Set the Size of the VBox
 		root.setMinSize(350, 250);
 		/*
-		 * * Set the padding of the VBox Set the border-style of the VBox Set the
+		 * Set the padding of the VBox Set the border-style of the VBox Set the
 		 * border-width of the VBox Set the border-insets of the VBox Set the
 		 * border-radius of the VBox Set the border-color of the VBox
 		 */
 		root.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;" + "-fx-border-width: 2;"
 				+ "-fx-border-insets: 5;" + "-fx-border-radius: 5;" + "-fx-border-color: blue;");
-// Create the Scene
+		// Create the Scene
 		Scene scene = new Scene(root);
-// Add the scene to the Stage
+		// Add the scene to the Stage
 		stage.setScene(scene);
-// Set the title of the Stage
-		stage.setTitle("A MenuButton Example");
-// Display the Stage
+		// Set the title of the Stage
+		stage.setTitle("A CheckBox Example");
+		// Display the Stage
 		stage.show();
-
 	}
 
 	// Helper Method
 	public void printMessage(String message) {
 		// Set the Text of the Label
-		messageLbl.setText(message);
+		selectionMsg.setText(message);
 	}
 
 	public static void main(String[] args) {
