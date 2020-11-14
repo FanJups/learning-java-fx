7e11ff4575c468365e37f98bd08510b17234f586
FanJups <jupsfan@gmail.com>
2020-11-14 18:57:30 +0100

    label

--- src/main/java/fan/jups/javafx/App.java
+++ src/main/java/fan/jups/javafx/App.java
@@ -1,38 +1,64 @@
 package fan.jups.javafx;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
 
-    private static Scene scene;
+	@Override
+	public void start(Stage stage) {
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+		// Create the Text Fields
+		TextField firstNameFld = new TextField();
+		TextField lastNameFld = new TextField();
+		// Create the Labels
+		Label firstNameLbl = new Label("_First Name:");
+		Label lastNameLbl = new Label("_Last Name:");
+		// Bind the Label to the according Field
+		firstNameLbl.setLabelFor(firstNameFld);
+		// Set mnemonic parsing to the Label
+		firstNameLbl.setMnemonicParsing(true);
+		// Bind the Label to the according Field
+		lastNameLbl.setLabelFor(lastNameFld);
+		// Set mnemonic parsing to the Label
+		lastNameLbl.setMnemonicParsing(true);
+		// Create the GridPane
+		GridPane root = new GridPane();
+		// Add the Labels and Fields to the GridPane
+		root.addRow(0, firstNameLbl, firstNameFld);
+		root.addRow(1, lastNameLbl, lastNameFld);
+		// Set the Size of the GridPane
+		root.setMinSize(350, 250);
+		/*
+		 * Set the padding of the GridPane Set the border-style of the GridPane Set the
+		 * border-width of the GridPane Set the border-insets of the GridPane Set the
+		 * border-radius of the GridPane Set the border-color of the GridPane
+		 */
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
+		root.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;" + "-fx-border-width: 2;"
+				+ "-fx-border-insets: 5;" + "-fx-border-radius: 5;" + "-fx-border-color: blue;");
+		// Create the Scene
+		Scene scene = new Scene(root);
+		// Add the scene to the Stage
+		stage.setScene(scene);
+		// Set the title of the Stage
+		stage.setTitle("A Label Example");
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+		// Display the Stage
+		stage.show();
+	}
 
-    public static void main(String[] args) {
-        launch();
-    }
+	public static void main(String[] args) {
+
+		launch();
+
+	}
 
 }
