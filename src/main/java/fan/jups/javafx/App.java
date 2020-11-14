f9ce6c10425147a9586e58948d3e9efd87d75946
FanJups <jupsfan@gmail.com>
2020-11-14 19:38:56 +0100

    menubutton

--- src/main/java/fan/jups/javafx/App.java
+++ src/main/java/fan/jups/javafx/App.java
@@ -4,9 +4,9 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -16,58 +16,60 @@ import javafx.stage.Stage;
 public class App extends Application {
 
 	// Create the Message Label
-	Label messageLbl = new Label("Press any Button to see the message");
+	Label messageLbl = new Label("Choose your car!");
 
 	@Override
 	public void start(Stage stage) {
 
-		// Create a normal button with N as its mnemonic
-		Button newBtn = new Button("_New");
-		// Add EventHandler to the Button
-		newBtn.setOnAction(new EventHandler<ActionEvent>() {
+		// Create the MenuItem ford
+		MenuItem ford = new MenuItem("Ford");
+		// Add EventHandler to the MenuItem
+		ford.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
-				printMessage("You have pressed the new Button");
+				printMessage("You have selected: Ford");
 			}
 		});
-		// Create a default button with S as its mnemonic
-		Button saveBtn = new Button("_Save");
-		// Set this Button as the Default
-		saveBtn.setDefaultButton(true);
-		// Add EventHandler to the Button
-		saveBtn.setOnAction(new EventHandler<ActionEvent>() {
+		// Create the MenuItem audi
+		MenuItem audi = new MenuItem("Audi");
+		// Add EventHandler to the MenuItem
+		audi.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
-				printMessage("You have pressed the save Button");
+				printMessage("You have selected: Audi");
 			}
 		});
-		// Create a cancel button with C as its mnemonic
-		Button cancelBtn = new Button("_Cancel");
-		cancelBtn.setCancelButton(true);
-		// Add EventHandler to the Button
-		cancelBtn.setOnAction(new EventHandler<ActionEvent>() {
+		// Create the MenuItem ferrari
+		MenuItem ferrari = new MenuItem("Ferrari");
+		// Add EventHandler to the MenuItem
+		ferrari.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
-				printMessage("You have pressed the cancel Button");
+				printMessage("You have selected: Ferrari");
 			}
 		});
-		// Create the HBox
-		HBox buttonBox = new HBox();
-		// Add the children to the HBox
-		buttonBox.getChildren().addAll(newBtn, saveBtn, cancelBtn);
-		// Set the vertical spacing between children to 15px
-		buttonBox.setSpacing(15);
+		// Create the MenuItem porsche
+		MenuItem porsche = new MenuItem("Porsche");
+		// Add EventHandler to the MenuItem
+		porsche.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				printMessage("You have selected: Porsche");
+			}
+		});
+		// Create the MenuButton
+		MenuButton cars = new MenuButton("Select");
+		// Add menu items to the MenuButton
+		cars.getItems().addAll(ford, audi, ferrari, porsche);
 		// Create the VBox
 		VBox root = new VBox();
 		// Add the children to the VBox
-		root.getChildren().addAll(messageLbl, buttonBox);
-		// Set the vertical spacing between children to 15px
-		root.setSpacing(15);
+		root.getChildren().addAll(cars, messageLbl);
 		// Set the Size of the VBox
 		root.setMinSize(350, 250);
 		/*
-		 * Set the padding of the VBox Set the border-style of the VBox Set the
-		 * border-width of the VBox * Set the border-insets of the VBox Set the
+		 * * Set the padding of the VBox Set the border-style of the VBox Set the
+		 * border-width of the VBox Set the border-insets of the VBox Set the
 		 * border-radius of the VBox Set the border-color of the VBox
 		 */
 		root.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;" + "-fx-border-width: 2;"
@@ -77,7 +79,7 @@ public class App extends Application {
 // Add the scene to the Stage
 		stage.setScene(scene);
 // Set the title of the Stage
-		stage.setTitle("A Button Example");
+		stage.setTitle("A MenuButton Example");
 // Display the Stage
 		stage.show();
 
